5310c51b160043a158f6e7c4588bbbd2eac21031
Charles Emley <ce_social@icloud.com>
2020-05-21 15:34:37 -0500

    Success for hello repeat and return string tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+    if (repetitions < 0) return 'ERROR'
+    let newString = ''
+    for (let i = 0; i < repetitions; i++) {
+        newString += string
+    }
+    return newString
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let length = string.length
+    let workingArray = string.split("")
+    let newString = ""
+    for (let i = length - 1; i > -1; i--) {
+        newString += workingArray[i]
+    }
+    return newString
 }
 
 module.exports = reverseString
